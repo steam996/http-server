6ff8d8e4fcf4918b9caa768b132d58be31c02800
Boris Andros <boris.andros96@gmail.com>
2023-01-17 00:32:34 +0300

    Изменена структура проекта

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,25 @@
 package ru.netology;
 
+import java.io.BufferedOutputStream;
+
 public class Main {
   public static void main(String[] args) {
+
+//    final var server = new Server();
+//    // код инициализации сервера (из вашего предыдущего ДЗ)
+//
+//    // добавление handler'ов (обработчиков)
+//    server.addHandler("GET", "/messages", new Handler() {
+//      public void handle(Request request, BufferedOutputStream responseStream) {
+//        // TODO: handlers code
+//      }
+//    });
+//    server.addHandler("POST", "/messages", new Handler() {
+//      public void handle(Request request, BufferedOutputStream responseStream) {
+//        // TODO: handlers code
+//      }
+//    });
+
     new Server().listen(9999);
   }
 }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -16,94 +16,23 @@ import java.util.concurrent.Executors;
 
 class Server {
 
-    public void listen(int port){
-        final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png",
-                "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html",
-                "/classic.html", "/events.html", "/events.js");
-        BlockingQueue <Runnable> queue = new ArrayBlockingQueue<>(1000);
-        try (final var serverSocket = new ServerSocket(port)) {
-            ExecutorService executeIt = Executors.newFixedThreadPool(64);
-           queue.put(() -> {
-                while (true) {
 
-                    try (
-                            final var socket = serverSocket.accept();
-                            final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                            final var out = new BufferedOutputStream(socket.getOutputStream());
-                    ) {
-                        // read only request line for simplicity
-                        // must be in form GET /path HTTP/1.1
-                        final var requestLine = in.readLine();
-                        final var parts = requestLine.split(" ");
-
-                        if (parts.length != 3) {
-                            // just close socket
-                            continue;
-                        }
-
-                        final var path = parts[1];
-                        if (!validPaths.contains(path)) {
-                            returnCode404(out);
-                            continue;
-                        }
-
-                        final var filePath = Path.of(".", "public", path);
-                        final var mimeType = Files.probeContentType(filePath);
-                        returnPage(path, out);
-                    } catch (NullPointerException | IOException e){
-                        continue;
-                    }
+    public void listen(int port) {
+        ExecutorService threadPool = Executors.newFixedThreadPool(64);
+        try(ServerSocket serverSocket = new ServerSocket(port)){
+            while (true){
+                try{
+                    ConnectionClient client = new ConnectionClient(serverSocket);
+                    threadPool.submit(client);
+                } catch (NullPointerException e){
+                    continue;
                 }
-            });
-           while (true){
-               executeIt.submit(queue.take());
-           }
-        } catch (IOException | InterruptedException e) {
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
-    private void returnCode404(BufferedOutputStream out) throws IOException {
-        out.write((
-                "HTTP/1.1 404 Not Found\r\n" +
-                        "Content-Length: 0\r\n" +
-                        "Connection: close\r\n" +
-                        "\r\n"
-        ).getBytes());
-        out.flush();
-    }
-
-    private void returnCode200(BufferedOutputStream out, Path filePath, long length) throws IOException {
-        final var mimeType = Files.probeContentType(filePath);
-        out.write((
-                "HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: " + mimeType + "\r\n" +
-                        "Content-Length: " + length + "\r\n" +
-                        "Connection: close\r\n" +
-                        "\r\n"
-        ).getBytes());
-    }
-
-    private void returnPage(String path, BufferedOutputStream out) throws IOException {
-        final var filePath = Path.of(".", "public", path);
-
-        // special case for classic
-        if (path.equals("/classic.html")) {
-            final var template = Files.readString(filePath);
-            final var content = template.replace(
-                    "{time}",
-                    LocalDateTime.now().toString()
-            ).getBytes();
-
-            returnCode200(out, filePath, content.length);
-            out.write(content);
-            out.flush();
-        } else {
-            final var length = Files.size(filePath);
-            returnCode200(out, filePath, length);
-            Files.copy(filePath, out);
-            out.flush();
-        }
-    }
 
 }
