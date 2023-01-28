450c017416a657af2474362eaea1fa22e86bb8f7
Борис Андрос <boris.andros96@gmail.com>
2023-01-29 01:27:55 +0300

    Добавлена обработка query параметров

--- src/main/java/ru/netology/ConnectionClient.java
+++ src/main/java/ru/netology/ConnectionClient.java
@@ -2,10 +2,7 @@ package ru.netology;
 
 import ru.netology.handlers.ResponseCodeSender;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -33,68 +30,37 @@ public class ConnectionClient {
                 "/classic.html", "/events.html", "/events.js");
         try (
 
-                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final var in = new BufferedInputStream(socket.getInputStream());
                 final var out = new BufferedOutputStream(socket.getOutputStream())
         ) {
-            System.out.println(Thread.currentThread().getName());
-            
-            // read only request line for simplicity
-            // must be in form GET /path HTTP/1.1
-            
+//
             var request = Request.parse(in, out, sender);
+            if (request == null) {
+                sender.sendCode404(out);
+                return;
+            }
 
-            if (!handlers.containsKey(request.getMethod())){
+            if (!handlers.containsKey(request.getMethod())) {
                 sender.sendCode404(out);
                 return;
             }
             var pathHandlers = handlers.get(request.getMethod());
-            if (!pathHandlers.containsKey(request.getPath())){
+            if (!pathHandlers.containsKey(request.getPath())) {
                 sender.sendCode404(out);
                 return;
             }
             var handler = pathHandlers.get(request.getPath());
             try {
                 handler.handle(request, out);
-            } catch (Exception e){
+            } catch (Exception e) {
                 sender.sendCode500(out);
                 System.out.println(e.getMessage());
             }
 
-
-            final var path = request.getPath();
-
-            if (!validPaths.contains(path)) {
-                sender.sendCode404(out);
-                return;
-            }
-
-            returnPage(path, out);
-        } catch (NullPointerException e){
-            return;
         } catch (IOException e) {
             e.printStackTrace();
-        
-        }
-    }
 
-    private void returnPage(String path, BufferedOutputStream out) throws IOException {
-        final var filePath = Path.of(".", "public", path);
-        // special case for classic
-        if (path.equals("/classic.html")) {
-            final var template = Files.readString(filePath);
-            final var content = template.replace(
-                    "{time}",
-                    LocalDateTime.now().toString()
-            ).getBytes();
-
-            sender.sendCode200(out, filePath, content.length);
-            out.write(content);
-            out.flush();
-        } else {
-            final var length = Files.size(filePath);
-            sender.sendCode200(out, filePath, length);
-            Files.copy(filePath, out);
-            out.flush();
         }
     }
 }
--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -1,18 +1,24 @@
 package ru.netology;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 import ru.netology.handlers.ResponseCodeSender;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.List;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class Request {
     private final String method;
     private final String path;
     private final String protocolVersion;
-    private final List <String> headers;
+    private final List<String> headers;
     private final byte[] body;
+    private static Map<String, List<String>> queryParams;
 
 
     private Request(String method, String path, String protocolVersion, List<String> headers, byte[] body) {
@@ -44,15 +50,124 @@ public class Request {
         return body;
     }
 
-    public static Request parse(BufferedReader in, BufferedOutputStream out, ResponseCodeSender sender) throws IOException {
-        final var requestLine = in.readLine();
-        final var parts = requestLine.split(" ");
-        System.out.println(requestLine);
-        if (parts.length != 3) {
-            sender.sendCode404(out);
-            throw new NullPointerException();
-        } else {
-            return new Request(parts[0], parts[1], parts[2], null, null);
+
+    public static Request parse(BufferedInputStream in, BufferedOutputStream out, ResponseCodeSender sender) throws IOException {
+        Request request;
+        // лимит на request line + заголовки
+        final var limit = 4096;
+
+        in.mark(limit);
+        final var buffer = new byte[limit];
+        final var read = in.read(buffer);
+
+        // ищем request line
+        final var requestLineDelimiter = new byte[]{'\r', '\n'};
+        final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        if (requestLineEnd == -1) {
+            ResponseCodeSender.sendCode400(out);
+            return null;
         }
+
+        // читаем request line
+        final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+        if (requestLine.length != 3) {
+            ResponseCodeSender.sendCode400(out);
+            return null;
+        }
+        final var method = requestLine[0];
+        var pathLine = requestLine[1].split("\\?");
+        final var path = pathLine[0];
+        if (pathLine.length > 1) {
+            getParams(pathLine[1]);
+        }
+
+        System.out.println(path);
+        System.out.println(method);
+
+
+        if (!path.startsWith("/")) {
+            ResponseCodeSender.sendCode400(out);
+            return null;
+        }
+
+        // ищем заголовки
+        final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+        final var headersStart = requestLineEnd + requestLineDelimiter.length;
+        final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+        if (headersEnd == -1) {
+            ResponseCodeSender.sendCode400(out);
+            return null;
+        }
+
+        // отматываем на начало буфера
+        in.reset();
+        // пропускаем requestLine
+        in.skip(headersStart);
+
+        final var headersBytes = in.readNBytes(headersEnd - headersStart);
+        final var headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+
+        // для GET тела нет
+        byte[] body = null;
+        if (!method.equals("GET")) {
+            in.skip(headersDelimiter.length);
+            // вычитываем Content-Length, чтобы прочитать body
+            final var contentLength = extractHeader(headers, "Content-Length");
+            if (contentLength.isPresent()) {
+                final var length = Integer.parseInt(contentLength.get());
+                final var bodyBytes = in.readNBytes(length);
+
+                body = bodyBytes;
+            }
+        }
+        return new Request(method, path, requestLine[2], headers, body);
+    }
+
+    private static void getParams(String paramLine) {
+        Map<String, List<String>> paramMap = new HashMap<>();
+
+        var paramList = URLEncodedUtils.parse(paramLine, StandardCharsets.UTF_8);
+        for (NameValuePair param : paramList) {
+            if (!paramMap.containsKey(param.getName())) {
+                paramMap.put(param.getName(), new ArrayList<>());
+            }
+            paramMap.get(param.getName()).add(param.getValue());
+        }
+        queryParams = paramMap;
+    }
+
+    private List<String> getParam(String name) {
+        return queryParams.get(name);
+    }
+
+    private static int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
+
+    private static Optional<String> extractHeader(List<String> headers, String header) {
+        return headers.stream()
+                .filter(o -> o.startsWith(header))
+                .map(o -> o.substring(o.indexOf(" ")))
+                .map(String::trim)
+                .findFirst();
+    }
+
+    private static String[] requestLineSearcher(BufferedOutputStream out, byte[] buffer, int read) throws IOException {
+        final var requestLineDelimiter = new byte[]{'\r', '\n'};
+        final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        if (requestLineEnd == -1) {
+            ResponseCodeSender.sendCode400(out);
+            return null;
+        }
+        return new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
     }
 }
